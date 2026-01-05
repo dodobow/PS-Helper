fe38b09b821b4b3b83f0342ffecfc08ec2bae8d1
DESKTOP-2CRI9KO\dodob <k01035675244@gmail.com>
2026-01-05 19:00:02 +0900

    Feat: 티어 출력 형식 변경  및 색상 반영

--- popup.js
+++ popup.js
@@ -1,23 +1,47 @@
+const TIER_COLORS = [
+    '#000000',
+    '#ad5600', '#b85b00', '#c46100', '#cf6700', '#db6c00',
+    '#435f7a', '#476582', '#4b6b8a', '#507292', '#54789a',
+    '#ec9a00', '#fba400', '#ffae00', '#ffb800', '#ffc300',
+    '#25d69b', '#27e2a4', '#28edac', '#2af8b4', '#2cffbc',
+    '#00b4fc', '#00c0ff', '#00ccff', '#00d8ff', '#00e4ff',
+    '#cc004e', '#dd0054', '#ee005b', '#ff0062', '#ff0068',
+    '#AC9FFF'
+];
+
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('button');
     const inputField = document.getElementById('solvedId');
+    const resultDiv = document.getElementById('result');
 
     button.addEventListener('click', async () => {
         const userId = inputField.value;
         if (!userId) {
             alert('ID를 입력해주세요.');
-            return
+            return;
         }
-        const url = `https://solved.ac/api/v3/user/show?handle=${userId}`
+        const url = `https://solved.ac/api/v3/user/show?handle=${userId}`;
         try {
             const response = await fetch(url);
             if (!response.ok) {
                 throw new Error('유저 정보를 가져올 수 없습니다.');
             }
             const data = await response.json();
-            console.dir(data);
             console.log(data);
-            alert(`연동에 성공했습니다. ${data.handle}님의 티어 점수는 ${data.tier}점, 레이팅은 ${data.rating}점입니다.`);
+            
+            let tierName = "Unrated";
+            if (0 < data.tier && data.tier < 31) {
+                tierName = ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Ruby'][Math.floor((data.tier - 1) / 5)] + ' ' + ['V', 'IV', 'III', 'II', 'I'][(data.tier + 4) % 5];
+            }
+            else if (data.tier == 31) {
+                tierName = 'Master';
+            }
+            let tierColor = TIER_COLORS[data.tier];
+            resultDiv.innerHTML = `
+            ${data.handle}<br>
+            레이팅 : <span style="color: ${tierColor}">${data.rating}</span><br>
+            티어 : <span style="color: ${tierColor}">${tierName}</span>
+            `
         } catch (error) {
             alert('오류가 발생했습니다. ' + error.message);
         }
